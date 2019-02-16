185544a8627bcaf1a65464d4703feeac34b7bd80
foxhound1971moto <foxhounf1971moto@me.com>
2019-02-16 08:06:20 +0000

    add index作成スクリプト実装およびES tweet格納ロジック実装

--- bin/updateESfromTimeLine.py
+++ bin/updateESfromTimeLine.py
@@ -7,7 +7,7 @@ Author : Sugimoto
 """
 
 # ES
-from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch import Elasticsearch, RequestsHttpConnection, helpers
 from requests_aws4auth import AWS4Auth
 
 # Twitter
@@ -18,6 +18,11 @@ import os
 import configparser
 import pprint
 import zc.lockfile
+import json
+from pytz import timezone
+from dateutil import parser
+from datetime import datetime
+import traceback
 
 def main():
     """
@@ -33,8 +38,8 @@ def main():
     
     """
     
-    pwd_path = os.getcwd() # カレントパス取得
-    basename = __file__ # ファイル名
+    pwd_path = os.path.dirname(os.path.abspath(__file__)) #パス取得
+    basename = os.path.basename(__file__) # ファイル名
     filename , ext = os.path.splitext(basename) #ファイル名と拡張子を分離
     
     conf_path = pwd_path + "/../conf/" + filename + ".conf"
@@ -47,33 +52,42 @@ def main():
     
     # ロックファイル生成
     lock = zc.lockfile.LockFile(loc_path)
-    
-    # ログレベル (デフォINFO, DEBUGフラグ有のみDEBUGレべル)
-    logging.basicConfig(level=logging.INFO)
-    if common_config.get("DEBUG") == 1:
-        logging.basicConfig(level=logging.DEBUG)
 
     # ロガー
     logger = logging.getLogger(filename)
-    
-    # ログのファイル出力先を設定
-    fh = logging.FileHandler(log_path)
-    logger.addHandler(fh)
-    
+
+    # ログファイルのフォーマット
+    fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
+
+    # ログレベル (デフォINFO, DEBUGフラグ有のみDEBUGレべル)
+    logging.basicConfig(filename=log_path, level=logging.INFO, format=fmt)
+    if common_config.get("DEBUG") == 1:
+        logging.basicConfig(filename=log_path, level=logging.DEBUG, format=fmt)
+
     # 各種設定値を格納
     params = {
         "conf_path": conf_path,
         "line_path": line_path,
         "logger" : logger
     }
+ 
+    logger.info("start")
     
-    tweets = getTweet (params)
-    result = updateElasticSearchbyTweets(tweets, params)
+    # tweet取得
+    tweets = getTweet(params)
+    # 新規tweet件数が更新されていればES更新
+    if len(tweets) == 0:
+        logger.info("no new tweet")
+    else:
+        # ES更新
+        result = updateElasticSearchbyTweets(tweets, params)
     
     # ロック解除
     lock.close()
     # ロックファイル削除
     os.remove(loc_path)
+    
+    logger.info("finish")    
     
     return
 
@@ -165,16 +179,17 @@ def getTweet (params):
     tweetConfig = getConfig("twitter", conf_path)
     lineConfig = getConfig("last_updated", line_path)
     
-    max_count = tweetConfig.get("max_count", conf_path)
-    api_key = tweetConfig.get("api_key", conf_path)
-    api_secret = tweetConfig.get("api_secret", conf_path)
-    access_token = tweetConfig.get("access_token", conf_path)
-    access_token_secret = tweetConfig.get("access_token_secret", conf_path)
-    
+    max_count = tweetConfig.get("max_count")
+    api_key = tweetConfig.get("api_key")
+    api_secret = tweetConfig.get("api_secret")
+    access_token = tweetConfig.get("access_token")
+    access_token_secret = tweetConfig.get("access_token_secret")
+    check_last_updated = tweetConfig.get("check_last_updated")
     last_max_id = lineConfig.get("last_max_id")
-    if len(last_max_id) <= 0:
+
+    # last_max_idが存在しなければNoneにする
+    if len(last_max_id) == 0:
         last_max_id = None
-    last_max_id = None
 
     tweets = []  #初期化
     try:
@@ -187,9 +202,9 @@ def getTweet (params):
         
         # ハンドラ
         api = tweepy.API(auth)
-        
-        if last_max_id != None:
-            max_id = last_max_id
+        # 前回取得idが存在する かつ check_last_updatedオプションがTrue
+        if last_max_id != None and check_last_updated == "True":
+            max_id = int(last_max_id)
             results = api.home_timeline(since_id=last_max_id, count=max_count) #前回のID以降のタイムライン取得
         else:
             results = api.home_timeline(count=max_count) #タイムライン取得
@@ -202,43 +217,41 @@ def getTweet (params):
             # max_id更新
             if max_id < result.id:
                 max_id = result.id 
-            
-           # pp.pprint(result)
-            print (result.user.name)
-            print (result.text)
-            print (result.created_at)
-            print (result.id)
-            print ("------\r\n")
+    
+            # pp.pprint(result)
+            # print (result.user.name)
+            # print (result.text)
+            # print (result.coordinates)
+            # print (result.is_quote_status)
+            # print (result.created_at)
+            # print (result.id)
+            # print ("------\r\n")
             
             # 必要な情報のみに絞る
             tweet = {
-                "id":               result.id,
+                "tweet_id":         result.id_str,
                 "user_name":        result.user.name,
                 "text":             result.text, 
-                "coordinates" :     result.coordinates,
-                "favorite_count" :  result.favorite_count,
-                "retweet_count"  :  result.retweet_count,
-                "is_quote_status" : result.is_quote_status, #リツイートかどうか(boolean)
-                "in_reply_to_screen_name" : result.in_reply_to_screen_name,
-                "created_at" :      result.created_at
+                "favorite_count" :  str(result.favorite_count),
+                "retweet_count"  :  str(result.retweet_count),
+                "is_quote_status" : str(result.is_quote_status), #リツイートかどうか(boolean)
+                "in_reply_to_screen_name" : str(result.in_reply_to_screen_name),
+                "created_at" :      str(result.created_at)
             }
+            logger.debug(tweet)
             
-            #lishに追加
+            #listに追加
             tweets.append(tweet)
             
         # last_max_id更新
         setConfig("last_updated", "last_max_id", str(max_id), line_path)
         
-        logger.debug(tweets)
-        
-        num = len(tweets)
-        logger.info("get " + str(len(tweets)) + " tweets" )
+        logger.info("get " + str(len(tweets)) + " tweets last_id:" + str(max_id))
         logger.info("finish collect tweets")
         
-    except tweepy.TweepError as e:
-        # エラー出力
-        print(e)
-        logging.warning(e)
+    except Exception as e:
+        #エラー出力
+        logger.warning(e)
     
     return tweets
     
@@ -268,24 +281,41 @@ def updateElasticSearchbyTweets (tweets, params):
     access_key = config.get("access_key")
     access_key_secret = config.get("access_key_secret")
     region = config.get("region")
-    print (access_key)
-    print (access_key_secret)
-    print (host)
-    # AWS認証セット
-    aws_auth = AWS4Auth(access_key, access_key_secret, region , 'es')
+    index_name = config.get("index")
+    type_name = config.get("type")
     
-    # ES接続
-    es = Elasticsearch(
-        hosts=[{'host': host, 'port': 443}],
-        http_auth=aws_auth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
-    
-    # ES 状態取得
-    print(es.info())
-    
+    try:
+        # AWS認証セット
+        aws_auth = AWS4Auth(access_key, access_key_secret, region , 'es')
+        
+        # ES接続
+        es = Elasticsearch(
+            hosts=[{'host': host, 'port': 443}],
+            http_auth=aws_auth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+        
+        # ESにTweetをバルクインサートするデータを作る
+        # 注意 type指定しているがES6以降複数type許容されなくなっていた...(ハマった)
+        # そのため、1index 1typeでしかデータを投入できないので注意
+        actions = []
+        for tweet in tweets:
+            actions.append({
+              "_index": index_name,
+              "_type": type_name,
+              "_id": tweet.get("str_id"),
+              "_source": tweet
+            })
+
+        # ESにTweetをバルクインサートする
+        logger.info(helpers.bulk(es, actions))
+        
+    except Exception as e:
+        #エラー出力
+        logger.warning(e)
+        
     return 0
 
 # 処理開始
